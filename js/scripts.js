8def889cd41f057d7f3aa7b04c7c7c072ecebae7
Anna Valkov & J.J. Idt <student@epicodus.com>
2014-07-17 12:20:08 -0700

    added third column for category and created list section for added categories

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,13 @@ var Purchase = {
 }
 
 $(document).ready(function(){
+
+  $("#add-button").click(function(){
+    var inputtedCategory = $("input#category-input").val();
+    $("#category-list").append("<li>"+inputtedCategory+"</li>");
+    $("input#category-input").val("");
+  });
+
   $("form#expenses").submit(function(event){
     var inputtedDescription = $("input#description").val();
     var inputtedAmount = parseInt($("input#amount").val());
@@ -17,8 +24,10 @@ $(document).ready(function(){
     $("table#purchases").append("<tr><td>" + newPurchase.describe + "</td><td>$ "
                                 + newPurchase.amount + "</td><td>" + newPurchase.quantity
                                 + "</td><td>$ " + newPurchase.totalCost() +"</td></tr>");
-
-
     event.preventDefault();
+  });
+
+  $("li").click(function(){
+    alert($(this).index());
   });
 });
